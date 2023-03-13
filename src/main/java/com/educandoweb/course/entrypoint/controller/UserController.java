06876e3adae3b224330574e2fbd2e5276032fbbe
Aline Paz <aline.soares.2@hotmail.com>
2023-03-13 15:36:43 -0300

    removendo imports

--- src/main/java/com/educandoweb/course/entrypoint/controller/UserController.java
+++ src/main/java/com/educandoweb/course/entrypoint/controller/UserController.java
@@ -6,17 +6,12 @@ import com.educandoweb.course.entrypoint.controller.request.UserRequest;
 import com.educandoweb.course.entrypoint.controller.response.UserResponse;
 import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
